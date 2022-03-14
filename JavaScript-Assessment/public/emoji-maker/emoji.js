18b365916bfd683c72547e128960f6abe3e7c5ec
Andreea Popa <andreea_popa22@yahoo.ro>
2022-03-14 13:10:12 +0200

    start js assessment

---
+++ JavaScript-Assessment/public/emoji-maker/emoji.js
@@ -0,0 +1,73 @@
+function yellowSkin(){
+    $('#skin').attr('src', './assets/skin/yellow.png');
+}
+
+function greenSkin(){
+    $('#skin').attr('src', './assets/skin/green.png');
+}
+
+function redSkin(){
+    $('#skin').attr('src', './assets/skin/red.png');
+}
+
+function selectEyesCard(){
+    $('#select-eyes-card').css('display', 'block');
+    $('#select-skin-card').css('display', 'none');
+    $('#select-mouth-card').css('display', 'none');
+}
+
+function selectMouthCard(){
+    $('#select-mouth-card').css('display', 'block');
+    $('#select-eyes-card').css('display', 'none');
+    $('#select-skin-card').css('display', 'none');
+}
+
+function selectSkinCard(){
+    $('#select-skin-card').css('display', 'block');
+    $('#select-eyes-card').css('display', 'none');
+    $('#select-mouth-card').css('display', 'none');
+}
+
+function normalEyes(){
+    $('#eyes').attr('src', './assets/eyes/normal.png');
+}
+
+function closedEyes(){
+    $('#eyes').attr('src', './assets/eyes/closed.png');
+}
+
+function longEyes(){
+    $('#eyes').attr('src', './assets/eyes/long.png');
+}
+
+function laughingEyes(){
+    $('#eyes').attr('src', './assets/eyes/laughing.png');
+}
+
+function rollingEyes(){
+    $('#eyes').attr('src', './assets/eyes/rolling.png');
+}
+
+function winkingEyes(){
+    $('#eyes').attr('src', './assets/eyes/winking.png');
+}
+
+function openMouth(){
+    $('#mouth').attr('src', './assets/mouth/open.png');
+}
+
+function sadMouth(){
+    $('#mouth').attr('src', './assets/mouth/sad.png');
+}
+
+function smilingMouth(){
+    $('#mouth').attr('src', './assets/mouth/smiling.png');
+}
+
+function straightMouth(){
+    $('#mouth').attr('src', './assets/mouth/straight.png');
+}
+
+function teethMouth(){
+    $('#mouth').attr('src', './assets/mouth/teeth.png');
+}
