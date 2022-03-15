e9d21cb1da8b411047d98358f65417bcfd4f32e2
Andreea Popa <andreea_popa22@yahoo.ro>
2022-03-15 16:28:59 +0200

    clean code

--- JavaScript-Assessment/public/quiz-app/quiz.js
+++ JavaScript-Assessment/public/quiz-app/quiz.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     var quizList = $('#quiz-list');
     var submitBtn = $('#submit-section');
     var scoreCount = $('#score-count');
@@ -9,61 +8,52 @@ $(document).ready(function() {
     submitBtn.remove();
     
     function createQuestion(id, question, answer, option){
-        // Crete Question Section
         var newQuestionSection = document.createElement('section');
         newQuestionSection.className = 'quiz-item'
         newQuestionSection.id = id;
         newQuestionSection.answer = answer;
 
-        // Create Question Text
         var newQuestionText = document.createElement('h3');
         var string = "Q" + id + ". " + question;
         var textNode = document.createTextNode(string);
         newQuestionText.appendChild(textNode);
-
         newQuestionSection.appendChild(newQuestionText)
 
-        // Create Question's option
         for (var i = 0; i<4; i++){
             var newOptionWrapper = document.createElement('div');
             newOptionWrapper.className = 'option-wrapper';
-
             var newLabelOption = document.createElement('label');
             var newInputOption = document.createElement('input');
             newInputOption.setAttribute('type', 'radio');
             newInputOption.setAttribute('name', 'q' + id);
             newInputOption.setAttribute('value', i);
             newInputOption.required = true;
+
             var newOptionText = document.createElement('p');
             var optionTextNode = document.createTextNode(option[i]);
-            newOptionText.appendChild(optionTextNode);
 
+            newOptionText.appendChild(optionTextNode);
             newLabelOption.appendChild(newInputOption);
             newLabelOption.appendChild(newOptionText);
-
             newOptionWrapper.appendChild(newLabelOption);
             newQuestionSection.appendChild(newOptionWrapper);
         }
-
         return newQuestionSection;
     }
 
+    
     function getQuestion(){
         $.get('http://5d76bf96515d1a0014085cf9.mockapi.io/quiz', function(data,status){
             var response = data;
-
             for (var i = 0; i < response.length; i++) {
                 quizList.append(createQuestion(response[i].id, response[i].question, response[i].answer, response[i].options))
                 answer.push(response[i].answer);
-                console.log(response[i])
             }
-            console.log(answer);
             quizList.append(submitBtn);
         })
     }
 
     getQuestion();
-
     console.log('Script Loaded');
 
     function checkAnswer(Form){
@@ -74,7 +64,6 @@ $(document).ready(function() {
             }
             i++
         });
-        console.log(score);
         scoreCount.html(score);
         score = 0;
     }
